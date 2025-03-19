a60c9b8f85a8658641be51c504cca097f7e23ad2
Muhammad Ali <155935067+muhammadali2828@users.noreply.github.com>
2025-03-19 22:31:58 +0500

    Add files via upload

---
+++ Lab Task 7/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+NASA_API = "PQJ1o3eHjBHbKQhYZOXMkFDubNXCkbFdqhDHMLOv"
+APOD_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY"
+
+@app.route("/", methods=["GET"])
+def index():
+    params = {"api_key": NASA_API} 
+    response = requests.get(APOD_URL, params=params)  
+    apod_data = response.json()
+
+    return render_template("index.html", apod=apod_data)
+
+if __name__ == "__main__":
+    app.run(debug=False)
